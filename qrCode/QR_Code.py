c530e4346866cfd4b2c0e50686ab204f4b014a19
habtamuadargaso <habtamu.adargaso@gmail.com>
2022-03-22 19:07:12 -0700

    create qr code

--- qrCode/QR_Code.py
+++ qrCode/QR_Code.py
@@ -2,8 +2,8 @@ import qrcode
 
 # Data for which you want to make QR code
 # Here we are using the URL of the MakeUseOf website
-data = "https://www.google.com/"
+data = "https://www.facebook.com/stories/129575584965605/UzpfSVNDOjExMzM4OTAwNDcxODI4NDY=/?bucket_count=9&source=story_tray"
 # Generating the QR code
 QRimage = qrcode.make(data)
 # Save to
-QRimage.save('myQr')
+QRimage.save('myQr.png')
